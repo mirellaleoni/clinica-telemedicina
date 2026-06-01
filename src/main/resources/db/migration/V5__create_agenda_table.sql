95b881c0a7f2dd050449385e06136189daae1cb2
iuvieun <naomininofelipe@gmail.com>
2026-06-01 20:35:41 -0300

    Update V5__create_agenda_table.sql

--- src/main/resources/db/migration/V5__create_agenda_table.sql
+++ src/main/resources/db/migration/V5__create_agenda_table.sql
@@ -6,6 +6,6 @@ CREATE TABLE agenda (
     tipo VARCHAR(50) NOT NULL,
     status VARCHAR(50) NOT NULL,
     criado_em TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    ADD CONSTRAINT fk_agenda_paciente FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
-    ADD CONSTRAINT fk_agenda_medico FOREIGN KEY (medico_id) REFERENCES medicos(id)
+    CONSTRAINT fk_agenda_paciente FOREIGN KEY (paciente_id) REFERENCES pacientes(id),
+    CONSTRAINT fk_agenda_medico FOREIGN KEY (medico_id) REFERENCES medicos(id)
 );
