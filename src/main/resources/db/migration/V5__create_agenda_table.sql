0840d1f8d933089614bf430a95b3f857deba7cdf
iuvieun <naomininofelipe@gmail.com>
2026-06-01 19:30:49 -0300

    renomeando os arquivos pra ficar padrao

---
+++ src/main/resources/db/migration/V5__create_agenda_table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE agendamentos (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    paciente_id UUID NOT NULL,
+    medico_id UUID NOT NULL,
+    data_hora TIMESTAMP NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_agendamentos_paciente
+        FOREIGN KEY (paciente_id)
+        REFERENCES pacientes(id),
+    CONSTRAINT fk_agendamentos_medico
+        FOREIGN KEY (medico_id)
+        REFERENCES medicos(id)
+);
