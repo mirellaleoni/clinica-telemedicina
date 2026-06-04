3123a8f6e0690a168ba56918581fd2244bf3e180
kaua <kauaxavier032@gmail.com>
2026-06-04 19:51:55 -0300

    feat: Adds endpoint to list patients in the controller.

--- src/main/java/com/clinica/presentation/PacienteController.java
+++ src/main/java/com/clinica/presentation/PacienteController.java
@@ -1,19 +1,25 @@
 package com.clinica.presentation;
 
 import com.clinica.application.CadastrarPacienteUseCase;
+import com.clinica.application.ListarPacientesUseCase;
+
 import com.clinica.infrastructure.PacienteEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
 
     private final CadastrarPacienteUseCase cadastrarPacienteUseCase;
+    private final ListarPacientesUseCase listarPacientesUseCase;
 
-    public PacienteController(CadastrarPacienteUseCase cadastrarPacienteUseCase) {
+    public PacienteController(CadastrarPacienteUseCase cadastrarPacienteUseCase, ListarPacientesUseCase listarPacientesUseCase) {
         this.cadastrarPacienteUseCase = cadastrarPacienteUseCase;
+        this.listarPacientesUseCase = listarPacientesUseCase;
     }
 
     @PostMapping
@@ -26,6 +32,13 @@ public class PacienteController {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(pacienteSalvo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PacienteEntity>> listar() {
+        List<PacienteEntity> pacientes = listarPacientesUseCase.executar();
+
+        return ResponseEntity.ok(pacientes);
     }
 
     public record CadastrarPacienteRequest(String nome, String cpf, String email) {}
