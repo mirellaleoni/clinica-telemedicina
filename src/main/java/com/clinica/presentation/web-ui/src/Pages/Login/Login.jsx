4e01abe697171587f43cb9af6235539c81d2b549
kaua <kauaxavier032@gmail.com>
2026-06-06 23:53:19 -0300

    feat: Implement login functionality with validation and CORS support

--- src/main/java/com/clinica/presentation/web-ui/src/Pages/Login/Login.jsx
+++ src/main/java/com/clinica/presentation/web-ui/src/Pages/Login/Login.jsx
@@ -1,12 +1,81 @@
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
+
+import { loginSchema } from "../../schemas/loginSchema.js";
+
+import { loginService } from "../../services/authService";
+
 import Button from "../../Components/Button/Button";
+import Input from "../../Components/Input/Input";
 
 function Login() {
+    const navigate = useNavigate();
+    const [serverError, setServerError] = useState("");
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(loginSchema)
+    });
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            navigate("/home");
+        }
+    }, [navigate]);
+
+    async function onSubmit(data) {
+        try {
+            setServerError("");
+            console.log(data);
+            const response = await loginService(data);
+            
+            localStorage.setItem("token", response.token); 
+            
+            navigate("/home");
+
+        } catch (error) {
+            console.error(error);
+            if (error.response && error.response.data) {
+                setServerError(error.response.data);
+            } else {
+                setServerError("Erro ao conectar com o servidor.");
+            }
+        }
+    }
+
     return (
-        <>
-            <h1>Login</h1>
-            <Button text="Entrar" onClick={() => {console.log("pamonha")}} />
-        </>
-    )
+        <main>
+            <h1>Sistema Médico - Login</h1>
+            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <Input 
+                        name="email"
+                        type="email"
+                        placeholder="Digite seu e-mail"
+                        register={register}
+                    />
+                    {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+                </div>
+
+                <div>
+                    <Input 
+                        name="senha"
+                        type="password"
+                        placeholder="Digite sua senha"
+                        register={register}
+                    />
+                    {errors.senha && <p style={{ color: 'red' }}>{errors.senha.message}</p>}
+                </div>
+
+                {serverError && <p style={{ color: 'red', fontWeight: 'bold' }}>{serverError}</p>}
+
+                <Button text="Entrar" type="submit" />
+            </form>
+        </main>
+    );
 }
 
 export default Login;
